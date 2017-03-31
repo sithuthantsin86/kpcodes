82ee8f33283cba872f6b0c9f5d132453dfb73f00
Mikhail Posypkin <mposypkin@gmail.com>
2017-03-31 23:12:22 +0300

    [fixed] avis generator

--- knapgenss.cpp
+++ knapgenss.cpp
@@ -9,6 +9,7 @@
 #include <random>
 #include <math.h>
 using namespace std;
+
 class Instances_generator_ss {
     int *p, *w, capacity;
     FILE *a, *b, *c, *d, *e;
@@ -22,7 +23,8 @@ public:
     void printPair(FILE* f, int *p, int *w, int N, int C);
     string file_name(string method, int N, int C, int num_instance);
 };
-Instances_generator_ss::Instances_generator_ss(int number_of_items){
+
+Instances_generator_ss::Instances_generator_ss(int number_of_items) {
     mkdir("inputs_ss", 0777);
     p = (int *) malloc(number_of_items * sizeof (int));
     if (p == NULL) {
@@ -35,11 +37,13 @@ Instances_generator_ss::Instances_generator_ss(int number_of_items){
         exit(1);
     }
 }
+
 void Instances_generator_ss::printPair(FILE* f, int *p, int *w, int N, int C) {
     fprintf(f, "%d\t%d\n", N, C);
     for (int i = 0; i < N; i++)
         fprintf(f, "%d\t%d\n", p[i], w[i]);
 }
+
 string Instances_generator_ss::file_name(string method, int N, int C, int num_instance) {
     ostringstream os;
     os << "inputs_ss/";
@@ -51,6 +55,7 @@ string Instances_generator_ss::file_name(string method, int N, int C, int num_in
     os << "_" << C;
     return os.str();
 }
+
 void Instances_generator_ss::pthree_instances(int number_of_items, int num_instances) {
     string M = "pthree_instances";
     mkdir("inputs_ss/pthree_instances", 0777);
@@ -60,16 +65,17 @@ void Instances_generator_ss::pthree_instances(int number_of_items, int num_insta
     std::mt19937 generator(seed);
     // set a distribution range (1 - 1000)
     std::uniform_int_distribution<int> distribution(1, 1000);
-    for (int j = 0; j < num_instances; j++){
-        for (int i = 0; i < number_of_items; i++){
+    for (int j = 0; j < num_instances; j++) {
+        for (int i = 0; i < number_of_items; i++) {
             p[i] = w[i] = distribution(generator);
         }
-        capacity = floor(number_of_items*(1000/4));
+        capacity = floor(number_of_items * (1000 / 4));
         a = fopen(file_name(M, number_of_items, capacity, j + 1).c_str(), "w");
         printPair(a, p, w, number_of_items, capacity);
         fclose(a);
     }
 }
+
 void Instances_generator_ss::psix_instances(int number_of_items, int num_instances) {
     string M = "psix_instances";
     mkdir("inputs_ss/psix_instances", 0777);
@@ -79,15 +85,16 @@ void Instances_generator_ss::psix_instances(int number_of_items, int num_instanc
     std::mt19937 generator(seed);
     // set a distribution range (1 - 1000000)
     std::uniform_int_distribution<int> distribution(1, 1000000);
-    for (int j = 0; j < num_instances; j++){
+    for (int j = 0; j < num_instances; j++) {
         for (int i = 0; i < number_of_items; i++)
             p[i] = w[i] = distribution(generator);
-        capacity = floor(number_of_items*(1000000/4));
+        capacity = floor(number_of_items * (1000000 / 4));
         b = fopen(file_name(M, number_of_items, capacity, j + 1).c_str(), "w");
         printPair(b, p, w, number_of_items, capacity);
         fclose(b);
     }
 }
+
 void Instances_generator_ss::evenodd_instances(int number_of_items, int num_instances) {
     string M = "evenodd_instances";
     mkdir("inputs_ss/evenodd_instances", 0777);
@@ -97,29 +104,29 @@ void Instances_generator_ss::evenodd_instances(int number_of_items, int num_inst
     std::mt19937 generator(seed);
     // set a distribution range (1 - 1000)
     std::uniform_int_distribution<int> distribution(1, 1000);
-    for (int j = 0; j < num_instances; j++){
+    for (int j = 0; j < num_instances; j++) {
         for (int i = 0; i < number_of_items; i++)
             p[i] = w[i] = distribution(generator);
-        capacity = 2 * floor(number_of_items*(1000/8)) + 1;
+        capacity = 2 * floor(number_of_items * (1000 / 8)) + 1;
         c = fopen(file_name(M, number_of_items, capacity, j + 1).c_str(), "w");
         printPair(c, p, w, number_of_items, capacity);
         fclose(c);
     }
 }
+
 void Instances_generator_ss::avis_instances(int number_of_items, int num_instances) {
     string M = "avis_instances";
     mkdir("inputs_ss/avis_instances", 0777);
-    for (int j = 0; j < num_instances; j++){
-        for (int i = 0; i < number_of_items; i++)
-            p[i] = w[i] = number_of_items * (number_of_items + 1) + i;
-        capacity = (number_of_items * (number_of_items + 1)) * floor((number_of_items - 1) / 2) + (number_of_items * (number_of_items - 1) / 2);
-        d = fopen(file_name(M, number_of_items, capacity, j + 1).c_str(), "w");
-        printPair(d, p, w, number_of_items, capacity);
-        fclose(d);
-    }
+    for (int i = 0; i < number_of_items; i++)
+        p[i] = w[i] = number_of_items * (number_of_items + 1) + i + 1;
+    capacity = (number_of_items * (number_of_items + 1)) * floor((number_of_items - 1) / 2) + (number_of_items * (number_of_items - 1) / 2);
+    d = fopen(file_name(M, number_of_items, capacity, 1).c_str(), "w");
+    printPair(d, p, w, number_of_items, capacity);
+    fclose(d);
 }
+
 void Instances_generator_ss::somatoth_instances(int number_of_items, int num_instances) {
-	int w1=0, w2=0;
+    int w1 = 0, w2 = 0;
     string M = "somatoth_instances";
     mkdir("inputs_ss/somatoth_instances", 0777);
     // obtain a seed from the system clock:
@@ -128,14 +135,14 @@ void Instances_generator_ss::somatoth_instances(int number_of_items, int num_ins
     std::mt19937 generator(seed);
     // set a distribution range (1 - number_of_items)
     std::uniform_int_distribution<int> distribution(1, number_of_items);
-    for (int j = 0; j < num_instances; j++){
-        for (int i = 0; i < number_of_items; i++){
+    for (int j = 0; j < num_instances; j++) {
+        for (int i = 0; i < number_of_items; i++) {
             w1 = distribution(generator);
-        	w2 = distribution(generator);
-        	if(j%2 == 0)
-        		p[i] = w[i] = ceil(j/2) * w1;
-        	else
-        		p[i] = w[i] = ceil(j/2) * w2;
+            w2 = distribution(generator);
+            if (j % 2 == 0)
+                p[i] = w[i] = ceil(j / 2) * w1;
+            else
+                p[i] = w[i] = ceil(j / 2) * w2;
         }
         capacity = (w1 - 1) * (w2 - 1) - 1;
         e = fopen(file_name(M, number_of_items, capacity, j + 1).c_str(), "w");
@@ -143,6 +150,7 @@ void Instances_generator_ss::somatoth_instances(int number_of_items, int num_ins
         fclose(e);
     }
 }
+
 int main(int argc, char* argv[]) {
     string method;
     struct stat st;
